ca9b98996f20d2247c8d487cca8ad69b459681f0
raczkowski <Reczk000wsk!!!pppp>
2019-08-09 22:20:07 +0200

    4: Excercise 12

--- scripts/listEnds.py
+++ scripts/listEnds.py
@@ -2,10 +2,24 @@
 
 import random
 
-def generateList():
+
+def generateRandomList():
     size = random.randint(5, 10)
     generatedNumbers = []
-    while size >= 0
+    while size >= 0:
         generatedNumbers.append(random.randint(5, 10))
-    return 
+        size -= 1
+    return generatedNumbers
 
+
+def getFirstAndLastIndexOf(list):
+    lastIndex = len(list) - 1
+    result = []
+    result.append(list[0])
+    result.append(list[lastIndex])
+    return result
+
+
+generatedList = generateRandomList()
+print(generatedList)
+print(getFirstAndLastIndexOf(generatedList))
