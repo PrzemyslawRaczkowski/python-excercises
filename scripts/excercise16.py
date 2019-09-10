9f809ae6a6b09ee17aafc5a27539c9006836ff33
raczkowski <Reczk000wsk!!!pppp>
2019-09-10 21:40:23 +0200

    Excercise 16 added

---
+++ scripts/excercise16.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.7
+
+def generateRandomPassword(passwordStrength):
+    return ""
+
+
+passwordStrength = input("How strong password do you want to be generated? Weak[W], Strong[S]")
+generateRandomPassword(passwordStrength)
